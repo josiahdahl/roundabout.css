70a408243ee60880e022c81b302700c99458e5cb
Josiah Dahl <josiah.dahl@gmail.com>
2016-03-04 15:46:48 -0800

    Works as expected, fairly alpha still

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/**/*')
+  .pipe(plumber((function(error) {
+    console.log(error.message);
+    this.emit('end');
+  })))
+  .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions', 'ie 10']
+  }))
+  .pipe(gulp.dest('./css'))
+  .pipe(gulp.dest('./demo/css'));
+});
+
+
+
+gulp.task('server', ['build'], function() {
+  browserSync({
+    open: false,
+    notify: true,
+    server: {
+      baseDir: './demo'
+    }
+  });
+});
+gulp.task('build', ['sass'], function() {
+  
+});
+
+gulp.task('default', ['server'], function() {
+  gulp.watch('./scss/**/*', ['sass', reload]);
+  gulp.watch('./demo/**/*', reload);
+});
